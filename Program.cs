3d844be8b4daff275a894091350750abc8325809
Asmaa <sayedasmaa2001@gmail.com>
2026-02-28 00:43:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n"+ new string('-',70) + "\n");
+            Console.WriteLine("Assignment 02 OOP");
+            Console.WriteLine("\n" + new string('-', 70) + "\n");
+
+        }
+    }
+}
